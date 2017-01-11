6f49e6d52eb00f091c0f55d5ed4fe215e1130dda
Raghu Betina <raghu@firstdraft.com>
2017-01-11 05:28:23 +0000

    Whitelist columns in Reward for ActiveAdmin

--- app/admin/reward.rb
+++ app/admin/reward.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Reward do
 
+ permit_params :reward_name, :reward_link
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
