6d8d7a4cfb9dfee3c529a96b1e7bd939b65e2db6
Raghu Betina <raghu@firstdraft.com>
2017-01-11 05:27:53 +0000

    Direct association between organizations and organizational_tasks

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,9 @@
 class Organization < ApplicationRecord
   # Direct associations
 
+  has_many   :organizational_tasks,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/organizational_task.rb
+++ app/models/organizational_task.rb
@@ -1,6 +1,8 @@
 class OrganizationalTask < ApplicationRecord
   # Direct associations
 
+  belongs_to :organization
+
   # Indirect associations
 
   # Validations
