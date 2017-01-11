27edcb8a43f9d7c3b38a2af5c56e918139e37e04
Raghu Betina <raghu@firstdraft.com>
2017-01-11 05:28:36 +0000

    Whitelist columns in VolunteerActivityLog for ActiveAdmin

--- app/admin/volunteer_activity_log.rb
+++ app/admin/volunteer_activity_log.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register VolunteerActivityLog do
 
+ permit_params :volunteer_id, :task_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
