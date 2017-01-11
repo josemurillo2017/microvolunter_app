cd0d69efc18c06878b5ee2d1fc655c3afb5975ae
Raghu Betina <raghu@firstdraft.com>
2017-01-11 05:27:28 +0000

    Generate volunteers resource with devise

---
+++ app/controllers/volunteers_controller.rb
@@ -0,0 +1,9 @@
+class VolunteersController < ApplicationController
+  def index
+    @volunteers = Volunteer.all
+  end
+
+  def show
+    @volunteer = Volunteer.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :volunteers
+  # Routes for the Volunteer resource:
+  # READ
+  get "/volunteers", :controller => "volunteers", :action => "index"
+  get "/volunteers/:id", :controller => "volunteers", :action => "show"
+
+
   devise_for :organizations
   # Routes for the Organization resource:
   # READ
