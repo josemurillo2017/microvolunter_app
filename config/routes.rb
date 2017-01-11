d6262f0b2286bd1d524a456feecd986d362b0f86
Raghu Betina <raghu@firstdraft.com>
2017-01-11 05:27:21 +0000

    Generate organizations resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :organizations
+  # Routes for the Organization resource:
+  # READ
+  get "/organizations", :controller => "organizations", :action => "index"
+  get "/organizations/:id", :controller => "organizations", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
