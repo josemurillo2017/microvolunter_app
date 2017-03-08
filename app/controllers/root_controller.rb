80e900f135eb69bdfe1b59083fe27a3cffe0907d
Jose Murillo <josemurillo2017@gmail.com>
2017-03-08 15:31:46 -0600

    Modified nav bar

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,11 +1,13 @@
 class RootController < ApplicationController
   def roots
-    if volunteer_signed_in?
+
+    if current_volunteer.present?
       redirect_to("/dashboard")
-    elsif organization_signed_in?
+    elsif current_organization.present?
       redirect_to("/org/dashboard")
     else
-      redirect_to("volunteers/sign_in")
+      redirect_to("/volunteers/sign_in")
     end
+
   end
 end
